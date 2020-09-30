51a01c454476a279a66dbb80b44d7baa68711e7a
mateusschwede <mateus.schwede@hotmail.com>
2020-09-30 10:57:28 -0300

    Animais finalizado

--- animais/uAnimais.pas
+++ animais/uAnimais.pas
@@ -9,7 +9,7 @@ uses
 
 type
   Tanimal = class(TForm)
-    Label1: TLabel;
+    lblIntro: TLabel;
     imgLeao: TImage;
     imgCavalo: TImage;
     imgHomem: TImage;
@@ -28,11 +28,19 @@ type
     rbtReptil: TRadioGroup;
     rbtAve: TRadioGroup;
     rbtNaoVoa: TRadioGroup;
+    btnReiniciar: TButton;
+    rbtMamifero: TRadioGroup;
+    rbtQuadrupede: TRadioGroup;
+    rbtBipede: TRadioGroup;
     procedure btnComecarClick(Sender: TObject);
     procedure rbtInicioClick(Sender: TObject);
     procedure rbtReptilClick(Sender: TObject);
     procedure rbtAveClick(Sender: TObject);
     procedure rbtNaoVoaClick(Sender: TObject);
+    procedure btnReiniciarClick(Sender: TObject);
+    procedure rbtMamiferoClick(Sender: TObject);
+    procedure rbtQuadrupedeClick(Sender: TObject);
+    procedure rbtBipedeClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -48,7 +56,6 @@ implementation
 
 procedure Tanimal.btnComecarClick(Sender: TObject);
 begin
-  {Imagens minimizadas}
   imgLeao.Visible := False;
   imgCavalo.Visible := False;
   imgHomem.Visible := False;
@@ -62,41 +69,182 @@ begin
   imgTartaruga.Visible := False;
   imgCrocodilo.Visible := False;
   imgCobra.Visible := False;
-
-  {RadioInicio visivel}
   rbtInicio.Visible := True;
+  btnComecar.Visible := False;
+  lblIntro.Visible := False;
+end;
+
+procedure Tanimal.btnReiniciarClick(Sender: TObject);
+begin
+  btnReiniciar.Visible := False;
+  imgLeao.Visible := True;
+  imgCavalo.Visible := True;
+  imgHomem.Visible := True;
+  imgMacaco.Visible := True;
+  imgMorcego.Visible := True;
+  imgBaleia.Visible := True;
+  imgAvestruz.Visible := True;
+  imgPingum.Visible := True;
+  imgPato.Visible := True;
+  imgAguia.Visible := True;
+  imgTartaruga.Visible := True;
+  imgCrocodilo.Visible := True;
+  imgCobra.Visible := True;
+  rbtInicio.Visible := False;
+  btnComecar.Visible := True;
+  lblIntro.Visible := True;
+  rbtInicio.Visible := False;
+  rbtInicio.ItemIndex := -1;
+  rbtReptil.Visible := False;
+  rbtReptil.ItemIndex := -1;
+  rbtAve.Visible := False;
+  rbtAve.ItemIndex := -1;
+  rbtNaoVoa.Visible := False;
+  rbtNaoVoa.ItemIndex := -1;
+  rbtMamifero.Visible := False;
+  rbtMamifero.ItemIndex := -1;
+  rbtQuadrupede.Visible := False;
+  rbtQuadrupede.ItemIndex := -1;
+  rbtBipede.Visible := False;
+  rbtBipede.ItemIndex := -1;
 end;
 
 procedure Tanimal.rbtAveClick(Sender: TObject);
 begin
-  if(rbtAve.ItemIndex=0) then rbtNaoVoa.Visible := True;
-  if(rbtAve.ItemIndex=1) then imgPato.Visible := True;
-  if(rbtAve.ItemIndex=2) then imgAguia.Visible := True;
+  if(rbtAve.ItemIndex=0) then
+  begin
+    rbtNaoVoa.Visible := True;
+    rbtAve.Visible := False;
+  end;
+  if(rbtAve.ItemIndex=1) then
+  begin
+    imgPato.Visible := True;
+    rbtAve.Visible := False;
+    btnReiniciar.Visible := True;
+  end;
+  if(rbtAve.ItemIndex=2) then
+  begin
+    imgAguia.Visible := True;
+    rbtAve.Visible := False;
+    btnReiniciar.Visible := True;
+  end;
+end;
+
+procedure Tanimal.rbtBipedeClick(Sender: TObject);
+begin
+  if(rbtBipede.ItemIndex=0) then
+  begin
+    rbtBipede.Visible := False;
+    imgHomem.Visible := True;
+    btnReiniciar.Visible := True;
+  end;
+  if(rbtBipede.ItemIndex=1) then
+  begin
+    rbtBipede.Visible := False;
+    imgMacaco.Visible := True;
+    btnReiniciar.Visible := True;
+  end;
 end;
 
 procedure Tanimal.rbtInicioClick(Sender: TObject);
 begin
-  if(rbtInicio.ItemIndex=2) then rbtReptil.Visible := True;
-  if(rbtInicio.ItemIndex=1) then rbtAve.Visible := True;
+  if(rbtInicio.ItemIndex=2) then
+  begin
+    rbtReptil.Visible := True;
+    rbtInicio.Visible := False;
+  end;
+  if(rbtInicio.ItemIndex=1) then
+  begin
+    rbtAve.Visible := True;
+    rbtInicio.Visible := False;
+  end;
 
   if(rbtInicio.ItemIndex=0) then
   begin
-    //Mamífero
+    rbtMamifero.Visible := True;
+    rbtInicio.Visible := False;
   end;
   
 end;
 
+procedure Tanimal.rbtMamiferoClick(Sender: TObject);
+begin
+  if(rbtMamifero.ItemIndex=0) then
+  begin
+    rbtQuadrupede.Visible := True;
+    rbtMamifero.Visible := False;
+  end;
+  if(rbtMamifero.ItemIndex=1) then
+  begin
+    rbtBipede.Visible := True;
+    rbtMamifero.Visible := False;
+  end;
+  if(rbtMamifero.ItemIndex=2) then
+  begin
+    rbtMamifero.Visible := False;
+    imgMorcego.Visible := True;
+    btnReiniciar.Visible := True;
+  end;
+  if(rbtMamifero.ItemIndex=3) then
+  begin
+    rbtMamifero.Visible := False;
+    imgBaleia.Visible := True;
+    btnReiniciar.Visible := True;
+  end;
+end;
+
 procedure Tanimal.rbtNaoVoaClick(Sender: TObject);
 begin
-  if(rbtNaoVoa.ItemIndex=0) then imgAvestruz.Visible := True;
-  if(rbtNaoVoa.ItemIndex=1) then imgPingum.Visible := True;
+  if(rbtNaoVoa.ItemIndex=0) then
+  begin
+    imgAvestruz.Visible := True;
+    rbtNaoVoa.Visible := False;
+    btnReiniciar.Visible := True;
+  end;
+  if(rbtNaoVoa.ItemIndex=1) then
+  begin
+    imgPingum.Visible := True;
+    rbtNaoVoa.Visible := False;
+    btnReiniciar.Visible := True;
+  end;
+end;
+
+procedure Tanimal.rbtQuadrupedeClick(Sender: TObject);
+begin
+  if(rbtQuadrupede.ItemIndex=0) then
+  begin
+    imgLeao.Visible := True;
+    rbtQuadrupede.Visible := False;
+    btnReiniciar.Visible := True;
+  end;
+  if (rbtQuadrupede.ItemIndex=1) then
+  begin
+    imgCavalo.Visible := True;
+    rbtQuadrupede.Visible := False;
+    btnReiniciar.Visible := True;
+  end;
 end;
 
 procedure Tanimal.rbtReptilClick(Sender: TObject);
 begin
-  if(rbtReptil.ItemIndex=0) then imgTartaruga.Visible := True;
-  if(rbtReptil.ItemIndex=1) then imgCrocodilo.Visible := True;
-  if(rbtReptil.ItemIndex=2) then imgCobra.Visible := True;
+  if(rbtReptil.ItemIndex=0) then
+  begin
+    imgTartaruga.Visible := True;
+    rbtReptil.Visible := False;
+    btnReiniciar.Visible := True;
+  end;
+  if(rbtReptil.ItemIndex=1) then
+  begin
+    imgCrocodilo.Visible := True;
+    rbtReptil.Visible := False;
+    btnReiniciar.Visible := True;
+  end;
+  if(rbtReptil.ItemIndex=2) then
+  begin
+    imgCobra.Visible := True;
+    rbtReptil.Visible := False;
+    btnReiniciar.Visible := True;
+  end;
 end;
 
 end.
